b1856552c69fa27af9a8bd767037a83850c2fac7
data2 <muskteerathos@gmail.com>
2020-07-10 17:36:18 +0800

    增加md

---
+++ kubernetes-md/src/main/java/com/data2/kubernetes/md/KubernetesMdApplication.java
@@ -0,0 +1,13 @@
+package com.data2.kubernetes.md;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesMdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesMdApplication.class, args);
+    }
+
+}
---
+++ kubernetes-md/src/test/java/com/data2/kubernetes/md/KubernetesMdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data2.kubernetes.md;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesMdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
